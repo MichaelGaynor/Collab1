52999bce91d42285d6fb0f875b2e01579a92cfd6
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-13 16:38:05 -0400

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function () {
+
+  var mypromise = $.ajax({
+    url: 'http://json-data.herokuapp.com/forms',
+    dataType: 'json',
+    success: function success(data) {
+      console.log(data);
+
+      var itemTemplate = $('#info-template').text();
+
+      var renderTemplate = _.template(itemTemplate);
+
+      _.each(data, function (i) {
+        var freshHTML = renderTemplate(i);
+        $('#TheScript').append(freshHTML);
+      });
+    }
+  });
+
+  // let url = 'http://json-data.herokuapp.com/forms';
+
+  // //Go fetch our dataand do something with it
+  // let promise = $.getJSON(url);
+  // promise.then(function (res){
+  //   doSomething(res);
+  // });
+
+  // //Templates
+  // let genericInput = _.template($('#generic-form').text());
+
+  // //Our do something function
+  // let doSomething = function(arr){
+
+  //   _.each(arr, function(item){
+
+  //     var htmlBlock;
+
+  //     if(item.type === 'text' || item.type === ''){
+  //       htmlBlock = genericInput(item);
+  //     }
+  //     $('form').append(htmlBlock);
+  //   })
+  // }
+})();
